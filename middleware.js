7b05f164f735394e6f48a751214ebe8dadcdaeb9
Upama <upamadolma@gmail.com>
2023-01-14 14:09:04 +0545

    authorization added

---
+++ middleware.js
@@ -0,0 +1,10 @@
+module.exports.isLoggedIn = (req, res, next) => {
+    console.log("REQ.USER...", req.user);
+    if (!req.isAuthenticated()) {
+        //store the url they are requesting
+        req.session.returnTo = req.originalUrl
+        req.flash('error', 'You must be signed in');
+        return res.redirect('/login');
+    }
+    next();
+}
