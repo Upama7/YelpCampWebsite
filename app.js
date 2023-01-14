322278e7b6a3581fc9c689ee7bd98a0e5e750959
Upama <upamadolma@gmail.com>
2023-01-14 14:07:03 +0545

    passport document used http://www.passportjs.org/packages/

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utilities/ExpressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 
-const campgrounds = require('./routes/campgrounds');
+const userRoutes = require('./routes/users');
+const campgroundsRoutes = require('./routes/campgrounds');
 //taking all campground routes and making a single route and exporting in main app.js file to make file look more clear
-const reviews = require('./routes/reviews');
+const reviewsRoutes = require('./routes/reviews');
 
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
@@ -46,8 +50,18 @@ const sessionConfig = {
     }
 }
 
+
 app.use(session(sessionConfig))
 app.use(flash());
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use( new LocalStrategy(User.authenticate())); //it is coming from passport document (http://www.passportjs.org/packages/ )
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 // //validate review form 
 // const validateReview = (req, res, next) => {
@@ -62,15 +76,24 @@ app.use(flash());
 
 
 app.use((req, res, next) => {
+    console.log(req.session);
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
 })
 
+app.get('/fakeUser', async (req, res) => {
+    const user = new User({ email: 'upama@gmail.com', username: 'Raiden'});
+    const newUser = await User.register(user, 'chicken');
+    res.send(newUser);
+})
+
 //router handler
 //campground router and name that we want to preffix with 
-app.use('/campgrounds', campgrounds)
-app.use('/campgrounds/:id/reviews/', reviews)
+app.use('/', userRoutes)
+app.use('/campgrounds', campgroundsRoutes)
+app.use('/campgrounds/:id/reviews/', reviewsRoutes)
 
 
 app.get('/', (req, res) => {
@@ -102,3 +125,16 @@ app.use((err, req, res, next) => {
 app.listen(3000, () => {
     console.log('Serving on port 3000')
 });
+
+
+
+
+
+
+
+
+
+
+
+
+//register - FORM ,  post / register - create a user, 
